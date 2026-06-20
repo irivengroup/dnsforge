70f408e79932db8ed1849b6bba3259c7db5985cc
Alfred TCHONDJO <altimagni@yahoo.fr>
2026-06-20 15:02:46 +0200

    DNSForce Manager integration

--- src/dnsforge/__init__.py
+++ src/dnsforge/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-__version__ = "14.5.2"
+__version__ = "14.5.6"
--- src/dnsforge_manager/__init__.py
+++ src/dnsforge_manager/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-__version__ = "14.4.0"
+__version__ = "14.5.6"
 
 __all__ = ["__version__"]
